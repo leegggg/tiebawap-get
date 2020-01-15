5cc6fc6d9458dddc20e17422d9d69132854f2d2e
LIN Yizhou <yizhou.lin@outlook.com>
2020-01-15 09:04:21 +0800

    random uuid

--- bin/getUpdate.sh
+++ bin/getUpdate.sh
@@ -7,4 +7,4 @@ fi
 cd /home/ylin/tiebawap-get
 source env/bin/activate
 # python -u src/tiebaWapGet/numberWatcher.py
-python -u src/tiebaWapGet/main.py -d sqlite:////home/ylin/anna/ext_hdd/by-uuid/aea3c7d1-7bf3-4028-a87f-b9dc140a6eec/all.[$1].tieba.baidu.com.db -k [$1] -a -b -t 250
+python -u src/tiebaWapGet/main.py -d sqlite:////home/ylin/anna/ext_hdd/by-uuid/aea3c7d1-7bf3-4028-a87f-b9dc140a6eec/all.$1.tieba.baidu.com.db -k $1 -a -b -t 250
