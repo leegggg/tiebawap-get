64369f0a370c6b09162e31dd26096a9f9db61c7a
LIN Yizhou <yizhou.lin@outlook.com>
2020-01-16 08:57:51 +0800

    use env

--- src/tiebaWapGet/req.py
+++ src/tiebaWapGet/req.py
@@ -1,14 +1,19 @@
 import requests
 from requests.adapters import HTTPAdapter
 from common import MAX_RETRY
+import os
 
-proxies = {
-  "http": "http://redqueen.lan.linyz.net:7890",
-  "https": "http://redqueen.lan.linyz.net:7890",
-}
+proxies = {}
+
+if os.environ.get("http_proxy"):
+    proxies["http"] = os.environ.get("http_proxy")
+
+if os.environ.get("https_proxy"):
+    proxies["https"] = os.environ.get("https_proxy")
 
 req = requests.Session()
-req.proxies.update(proxies)
+if proxies:
+    req.proxies.update(proxies)
 httpAdapter = HTTPAdapter(max_retries=MAX_RETRY)
 
 req.mount('http://', httpAdapter)
